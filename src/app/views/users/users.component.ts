47b4671ac109eb1fb5e4dfbd24a6228c2179881f
Dvyd <david.llopislaguna@gmail.com>
2024-02-19 17:22:14 +0100

    On branch dvyd Your branch is up to date with 'origin/dvyd'.
    Changes to be committed:
    	modified:   src/app/core/helpers/storage.helper.ts
    	modified:   src/app/views/users/users.component.html
    	modified:   src/app/views/users/users.component.ts
    	modified:   src/index.html

--- src/app/views/users/users.component.ts
+++ src/app/views/users/users.component.ts
@@ -4,13 +4,14 @@ import { JsonPipe, AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LoaderComponent } from "@/app/shared/components/loader/loader.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-users',
     standalone: true,
     templateUrl: './users.component.html',
     styleUrl: './users.component.scss',
-    imports: [JsonPipe, AsyncPipe, LoaderComponent]
+    imports: [JsonPipe, AsyncPipe, LoaderComponent, RouterLink]
 })
 export class UsersComponent {
   private api = inject(ApiService);
