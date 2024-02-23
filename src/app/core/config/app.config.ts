4fefb89a82d37e4be42afe17bf7553fb540b0621
Rocio <rciperezmlina1@gmail.com>
2024-02-23 13:43:54 +0100

    On branch xio Your branch is up to date with 'origin/xio'.
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app/app.component.ts
    	modified:   src/app/core/config/app.config.ts
    	modified:   src/app/core/services/storage.service.ts
    	modified:   src/app/views/home/home.component.html
    	modified:   src/app/views/home/home.component.scss
    	modified:   src/app/views/home/home.component.ts
    	new file:   src/assets/i18n/en.json
    	new file:   src/assets/i18n/es.json
    	new file:   "src/assets/images/svg/banderaEspa\303\261ola.svg"
    	new file:   src/assets/images/svg/banderaInglesa.svg

--- src/app/core/config/app.config.ts
+++ src/app/core/config/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import {
   provideRouter,
   withComponentInputBinding,
@@ -17,6 +17,32 @@ import {
 import { errorResponseInterceptorFn } from '@/app/core/interceptors/error-response.interceptor';
 import { tokenInterceptorFn } from '@/app/core/interceptors/token-fn.interceptor';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { HttpClient } from '@angular/common/http';
+import {
+  TranslateService,
+  TranslateLoader,
+  TranslateModule,
+} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideRouter(routes, withViewTransitions(), withComponentInputBinding()),
+//     provideClientHydration(
+//       withHttpTransferCacheOptions({
+//         includePostRequests: true,
+//       })
+//     ),
+//     provideHttpClient(
+//       withFetch(),
+//       withInterceptors([errorResponseInterceptorFn, tokenInterceptorFn])
+//     ),
+//     provideNoopAnimations(),
+//   ],
+// };
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -31,5 +57,15 @@ export const appConfig: ApplicationConfig = {
       withInterceptors([errorResponseInterceptorFn, tokenInterceptorFn])
     ),
     provideNoopAnimations(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      })
+    ),
+    TranslateService,
   ],
 };
