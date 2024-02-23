3f03b5d4cdae8304fe39c74bd3ba4dce6380430a
Rocio <rciperezmlina1@gmail.com>
2024-02-23 13:43:54 +0100

    On branch xio Your branch is up to date with 'origin/xio'.
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app/app.component.ts
    	modified:   src/app/core/config/app.config.ts
    	modified:   src/app/core/services/storage.service.ts
    	modified:   src/app/views/home/home.component.html
    	modified:   src/app/views/home/home.component.scss
    	modified:   src/app/views/home/home.component.ts
    	new file:   src/assets/i18n/en.json
    	new file:   src/assets/i18n/es.json
    	new file:   "src/assets/images/svg/banderaEspa\303\261ola.svg"
    	new file:   src/assets/images/svg/banderaInglesa.svg

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,6 +1,10 @@
 import { FooterComponent } from '@/app/shared/layout/components/footer/footer.component';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { home as langEs } from '@/assets/i18n/es.json';
+import { home as langEn } from '@/assets/i18n/en.json';
+// import * as langEs from '@/assets/i18n/es.json';
+// import * as langEn from '@/assets/i18n/en.json';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +13,24 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  idioma = 'castellano';
+  textos: any;
+
+  ngOnInit(): void {
+    this.setIdioma();
+  }
+
+  setIdioma() {
+    if (this.idioma === 'castellano') {
+      this.textos = langEs;
+    } else {
+      this.textos = langEn;
+    }
+  }
+
+  cambiarIdioma(idiomaSeleccionado: string) {
+    this.idioma = idiomaSeleccionado;
+    this.setIdioma();
+  }
+}
