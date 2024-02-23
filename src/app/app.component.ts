9b42df035e90562435d5414f773b117077272da1
Rocio <rciperezmlina1@gmail.com>
2024-02-23 13:43:54 +0100

    On branch xio Your branch is up to date with 'origin/xio'.
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app/app.component.ts
    	modified:   src/app/core/config/app.config.ts
    	modified:   src/app/core/services/storage.service.ts
    	modified:   src/app/views/home/home.component.html
    	modified:   src/app/views/home/home.component.scss
    	modified:   src/app/views/home/home.component.ts
    	new file:   src/assets/i18n/en.json
    	new file:   src/assets/i18n/es.json
    	new file:   "src/assets/images/svg/banderaEspa\303\261ola.svg"
    	new file:   src/assets/images/svg/banderaInglesa.svg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +11,4 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
   public title = 'FactivarWeb';
-
-  constructor(private translate: TranslateService) {
-    translate.setDefaultLang('es');
-    translate.use('en');
-  }
 }
