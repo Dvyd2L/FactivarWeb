ea6ae0a93bbb7799a0a4416f4aaefc6487f17daa
Dvyd <david.llopislaguna@gmail.com>
2024-02-27 21:49:46 +0100

    On branch dvyd Your branch is up to date with 'origin/dvyd'.
    Changes to be committed:
    	modified:   src/app/shared/layout/layout.component.html
    	modified:   src/app/shared/layout/layout.component.ts
    	modified:   src/app/views/auth/auth.component.html

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ViewChild,
+  ViewContainerRef,
+  inject,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ToastService } from '@/app/core/services/toast.service';
 
 @Component({
   selector: 'app-layout',
@@ -11,7 +18,10 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {
+export class LayoutComponent implements AfterViewInit {
+  private readonly toastSvc = inject(ToastService);
+  @ViewChild('toastContainer', { read: ViewContainerRef })
+  public toast!: ViewContainerRef;
   public toggleMenu() {
     const $sidebar = document.querySelector('#sidebar') as HTMLElement;
     const $layout = document.querySelector('#layout') as HTMLElement;
@@ -22,5 +32,8 @@ export class LayoutComponent {
 
     $layout.style.gridTemplateColumns =
       $layout.style.gridTemplateColumns === '0fr 6fr' ? '1fr 5fr' : '0fr 6fr';
+  }
+  ngAfterViewInit(): void {
+    this.toastSvc.show(this.toast);
   }
 }
