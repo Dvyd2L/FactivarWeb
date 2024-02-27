c40978556b51d12ca293ed24249f27d38bb30c77
Dvyd <david.llopislaguna@gmail.com>
2024-02-27 13:21:36 +0100

    On branch dvyd Your branch and 'origin/dvyd' have diverged, and have 10 and 4 different commits each, respectively.   (use "git pull" to merge the remote branch into yours)
    Changes to be committed:
    	modified:   src/app/shared/components/cards-color/cards-color.component.html
    	modified:   src/app/shared/components/cards-color/cards-color.component.scss
    	modified:   src/app/shared/components/cards-color/cards-color.component.ts

--- src/app/shared/components/cards-color/cards-color.component.ts
+++ src/app/shared/components/cards-color/cards-color.component.ts
@@ -1,11 +1,12 @@
 import { IInvoice } from '@/app/models/interfaces/api';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cards-color',
   standalone: true,
-  imports: [DatePipe, CurrencyPipe],
+  imports: [DatePipe, CurrencyPipe, RouterLink],
   templateUrl: './cards-color.component.html',
   styleUrl: './cards-color.component.scss'
 })
