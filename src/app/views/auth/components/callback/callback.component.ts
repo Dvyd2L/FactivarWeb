fa886f1a455c0eaebf12a36f3ded79a7014d3522
Dvyd <david.llopislaguna@gmail.com>
2024-03-03 18:08:04 +0100

    On branch dvyd Your branch is up to date with 'origin/dvyd'.
    Changes to be committed:
    	modified:   src/app/views/auth/components/callback/callback.component.ts

--- src/app/views/auth/components/callback/callback.component.ts
+++ src/app/views/auth/components/callback/callback.component.ts
@@ -1,11 +1,5 @@
-import { StorageService } from '@/app/core/services/storage.service';
-import { UserService } from '@/app/core/services/user.service';
-import { RolesEnum } from '@/app/models/enums/roles.enum';
-import { StorageKeyEnum } from '@/app/models/enums/storage.enum';
-import { IUser } from '@/app/models/interfaces/user';
 import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { JwtHelperService } from '@auth0/angular-jwt';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -18,8 +12,6 @@ import { AuthService } from '../../services/auth.service';
 export class CallbackComponent implements OnInit {
   private readonly router = inject(Router);
   private readonly auth = inject(AuthService);
-  private readonly storage = inject(StorageService);
-  private readonly userSvc = inject(UserService<IUser>);
 
   ngOnInit(): void {
     this.getUrl();
@@ -27,22 +19,7 @@ export class CallbackComponent implements OnInit {
 
   private getUrl() {
     const currentUrl = this.router.url;
-    const token = currentUrl.split('id_token=')[1].split('&')[0];
-    // const jwtHelper = new JwtHelperService();
-    // const payload = jwtHelper.decodeToken(token);
-    // this.storage.set(StorageKeyEnum.Token, token);
-    // this.userSvc.updateUser({
-    //   role: RolesEnum.User,
-    //   sid: crypto.randomUUID(),
-    //   token,
-    //   aud: payload.aud,
-    //   email: payload.email,
-    //   exp: payload.exp,
-    //   iss: payload.iss,
-    //   name: payload.given_name,
-    //   thumbprint: payload.picture,
-    //   surname: payload.family_name,
-    // });
+    const token = currentUrl?.split('id_token=')[1]?.split('&')[0];
     this.auth.loginWithGoogle(token).subscribe();
     this.router.navigate(['/']);
   }
